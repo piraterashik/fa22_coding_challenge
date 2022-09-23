5519f597708b0a1e0be31159c871d29935cd4515
piraterashik <98787027+piraterashik@users.noreply.github.com>
2022-09-22 23:34:36 -0400

    Update tasks.js

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -24,4 +24,33 @@ router.get("/", async (req, res) => {
 
 /* CREATE 'DELETE' REQUEST */
 
+router.put('/task/:id', (req, res) => {
+  Task.findOneAndUpdate({
+    _id: req.params.id
+  },
+  {$set: {title: req.body.title }},
+  { upsert: true},
+  function(err,newTask){
+    if(err){
+      console.log('error occured');
+    }else {
+      console.log(newTask);
+      res.send(newTask);
+    }
+  });
+});
+
+router.delete('/task/:id', (req, res) => {
+  Task.findOneAndRemove({
+  _id: req.params.id
+},function(err,task){
+  if(err){
+    res.send('error deleting');
+  }else {
+    console.log(task);
+    res.status(204);
+  }
+});
+});
+
 module.exports = router;
