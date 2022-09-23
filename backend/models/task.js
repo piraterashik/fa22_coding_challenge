dee59247bdd4a31cdc97aca8c9719d6bec53db91
piraterashik <98787027+piraterashik@users.noreply.github.com>
2022-09-22 23:33:53 -0400

    Update task.js

--- backend/models/task.js
+++ backend/models/task.js
@@ -2,5 +2,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 /* CREATE 'taskSchema' */
+var taskSchema = new Schema({
+    title: { type: String, required: true }
+})
+
 
 module.exports = mongoose.model("task", taskSchema);
